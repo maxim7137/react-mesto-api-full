5e81a5dcc05a1a8597757671b216cb047f237288
maxim7137 <maxim713@mail.ru>
2023-01-01 17:31:37 +0300

    feature: crash-test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,12 @@ const { createUser, login } = require('../controllers/users');
 
 const { validateRegUser, validateLoginUser } = require('../middlewares/validations');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signup', validateRegUser, createUser);
 router.post('/signin', validateLoginUser, login);
 
