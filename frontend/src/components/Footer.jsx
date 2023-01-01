394c1c15adb9e2e1c8a36e490f1fb5fb0c19ae3a
maxim7137 <maxim713@mail.ru>
2023-01-01 16:26:54 +0300

    feature: add publish

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">&copy; {new Date().getFullYear()} Mesto Russia</p>
+      <p className="footer__text">
+        &copy; {new Date().getFullYear()} Mesto Russia
+      </p>
     </footer>
   );
 }
