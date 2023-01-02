66b33c529e0b3bf3ab060564d528e0a35250ce88
maxim7137 <maxim713@mail.ru>
2023-01-02 17:43:00 +0300

    feature: after review

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,11 +7,13 @@ const {
   validateLoginUser,
 } = require('../middlewares/validations');
 
+/* для ревью проверка сервера
 router.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+для ревью проверка сервера */
 
 router.post('/signup', validateRegUser, createUser);
 router.post('/signin', validateLoginUser, login);
